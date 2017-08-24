b394b1702efde182f80a1279e6229e2b5d33649e
TimothyJohnK <Tim@VectorLab.com>
2017-08-24 00:56:57 -0500

    red quilted bkg; and no color for header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>King Baron Hot Club</h1>
+      </div>
+    );
+  }
+}
+class Button extends Component {
+  render() {
+    return (
+      <div className="navButton">
+        {" "}{this.props.name}{" "}
+      </div>
+    );
+  }
+}
+
+class MainContent extends Component {
+  render() {
+    return <div className="mainContent"> </div>;
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navBar">
+        <Button name="News" />
+        <Button name="Contact" />
+        <Button name="Promo/Bio" />
+        <Button name="Calendar" />
+        <Button name="Gallery" />
+        <Button name="Music" />
+        <Button name="Songbook" />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Header />
+        <div className="mainWrapper">
+          <NavBar />
+          <MainContent />
+          <div className="picture" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
