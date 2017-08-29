fe25f79f046bf1860e85c06a8b4dd9ebf18ae485
TimothyJohnK <Tim@VectorLab.com>
2017-08-29 16:44:26 -0500

    big commit; TODO sticky footer stay at bottom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const NewsMain;
-const ContactMain;
-const
-const
-const
-const
-const
-
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="clarinet" />
         <h1>King Baron Hot Club</h1>
-        <div className="guitar" />
+      </div>
+    );
+  }
+}
+
+class NewsMain extends Component {
+  render() {
+    return (
+      <div>
+        <p>Just some fillers; lorem ipsum</p>
+        <p>Just some fillers; lorem ipsum</p>
+        <p>Just some fillers; lorem ipsum</p>
+      </div>
+    );
+  }
+}
+class ContactMain extends Component {
+  render() {
+    return (
+      <div>
+        <p>ContactMain</p>
+        <p>Just some fillers; lorem ipsum</p>
+      </div>
+    );
+  }
+}
+class PromoMain extends Component {
+  render() {
+    return (
+      <div>
+        PromoMainPromoMainPromoMainPromoMain
+        <p>Just some fillers; lorem ipsum</p>
+        <p>Just some fillers; lorem ipsum</p>
+      </div>
+    );
+  }
+}
+class CalendarMain extends Component {
+  render() {
+    return (
+      <div>
+        <p>CalendarMain</p>
+      </div>
+    );
+  }
+}
+class GalleryMain extends Component {
+  render() {
+    return (
+      <div>
+        <p>GalleryMain</p>
+      </div>
+    );
+  }
+}
+class MusicMain extends Component {
+  render() {
+    return (
+      <div>
+        <p>MusicMain</p>
+      </div>
+    );
+  }
+}
+class SongbookMain extends Component {
+  render() {
+    return <div> SongbookMain </div>;
+  }
+}
+
+class Footer extends Component {
+  render() {
+    const year = new Date();
+    return (
+      <div className="footer">
+        <div>Some sort of thing here</div>
+        <div className="year">
+          <h3>©{year.getFullYear()}</h3>
+        </div>
+        <div>
+          <a href="mailto: kingbaron@gmail.com" className="email">
+            kingbaron@gmail.com
+          </a>
+        </div>
       </div>
     );
   }
@@ -24,59 +98,80 @@ class Header extends Component {
 class Button extends Component {
   render() {
     return (
-      <div className="navButton">
-        {" "}{this.props.name}{" "}
+      <div className="navButton" onClick={this.props.onClick}>
+        {" "}
+        {this.props.name}{" "}
+      </div>
+    );
+  }
+}
+
+class Viewer extends Component {
+  render() {
+    return <div className="viewer">{this.props.view}</div>;
+  }
+}
+const componentArray = [
+  { name: "news", content: <NewsMain /> },
+  { name: "contact", content: <ContactMain /> },
+  { name: "promo", content: <PromoMain /> },
+  { name: "calendar", content: <CalendarMain /> },
+  { name: "gallery", content: <GalleryMain /> },
+  { name: "music", content: <MusicMain /> },
+  { name: "songbook", content: <SongbookMain /> }
+];
+class NavBar extends Component {
+  handleClick() {
+    if (this.props.name === componentArray.name) {
+      const filteredComponent = componentArray.filter(
+        content => this.props.content
+      );
+      return this.setState({ filteredComponent });
+    }
+  }
+  render() {
+    return (
+      <div className="navBar">
+        <Button name="news" onClick={this.handleClick} />
+        <Button name="contact" onClick={this.handleClick} />
+        <Button name="promo" onClick={this.handleClick} />
+        <Button name="calendar" onClick={this.handleClick} />
+        <Button name="gallery" onClick={this.handleClick} />
+        <Button name="music" onClick={this.handleClick} />
+        <Button name="songbook" onClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+class MainWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = <NewsMain />;
+  }
+
+  render() {
+    return (
+      <div className="mainWrapper">
+        <NavBar />
+        <Viewer view={this.state} />
       </div>
     );
   }
 }
 
 /* TODO
-  set state in MainContent; make function to handleClick
+  set state in MainWrapper; make function to handleClick
   set state in NavBar to keep current button state highlighted; style held down click to look different than button:hover
+  Have component MainWrapper call all of it's children content
 */
-
-
-class MainContent extends Component {
-
-  render() {
-    return (
-      <div className="mainContent">A bunch of hardcoded bio text and shizz</div>
-    );
-  }
-}
-
-class Space extends Component {
-  render() {
-    return <div className="space" />;
-  }
-}
-
-class NavBar extends Component {
-  render() {
-    return (
-      <div className="navBar">
-        <Button name="News" />
-        <Button name="Contact" />
-        <Button name="Promo/Bio" />
-        <Button name="Calendar" />
-        <Button name="Gallery" />
-        <Button name="Music" />
-        <Button name="Songbook" />
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <div className="mainWrapper">
-          <NavBar />
-          <MainContent />
-        </div>
+        <MainWrapper />
+        <Footer />
       </div>
     );
   }
